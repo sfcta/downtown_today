e7b200459745fcd46be68b39d5a6ae16daa2c34d
CHOW Chun Ho <chunhochow@users.noreply.github.com>
2024-07-05 17:08:56 -0700

    utils separate out add_normalization_col; population, race_ethnicity, commute_mode_choice init

--- utils.py
+++ utils.py
@@ -1,6 +1,23 @@
 import polars as pl
 
 
+def add_normalization_col(df, x, y, normalized_y_col_name, norm_x_value=2019):
+    norm_col = f"{y}-at_{norm_x_value}"
+    join_on = df.select(pl.col("*").exclude(x, y, norm_col)).columns
+    return (
+        df.join(
+            df.filter(pl.col(x) == norm_x_value)
+            .rename({y: norm_col})
+            .select(join_on + [norm_col]),
+            on=join_on,
+            how="left",
+            coalesce=True,
+        )
+        .with_columns((pl.col(y) / pl.col(norm_col)).alias(normalized_y_col_name))
+        .drop(norm_col)
+    )
+
+
 def add_industry_share_col(df, col):
     """for CES and QCEW"""
     return df.with_columns(
@@ -15,15 +32,11 @@ def line_plot(df, x, y, by, title, frame_width=None):
 
 
 def line_plot_normalized(df, x, y, by, title, norm_x_value=2019, frame_width=None):
-    norm_col = f"{x}-{norm_x_value}"
-    normalized_y_col = f"{y} (normalized to {norm_x_value})"
-    df = df.join(
-        df.filter(pl.col(x) == norm_x_value).rename({y: norm_col}),
-        on=by,
-        how="left",
-        coalesce=True,
-    ).with_columns((pl.col(y) / pl.col(norm_col)).alias(normalized_y_col))
-    return line_plot(df, x, normalized_y_col, by, title=title, frame_width=frame_width)
+    normalized_y_col_name = f"{y}-normalized_to_{norm_x_value}"
+    add_normalization_col(df, x, y, normalized_y_col_name, norm_x_value=norm_x_value)
+    return line_plot(
+        df, x, normalized_y_col_name, by, title=title, frame_width=frame_width
+    )
 
 
 def stacked_bar_plot(df, y, geography, by, title, years, frame_width=None):
