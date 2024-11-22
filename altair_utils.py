e6d512fdca5eb49295d5b6db38026abab20a0cfa
CHOW Chun Ho <chunhochow@users.noreply.github.com>
2024-11-21 17:01:19 -0800

    utils: line plot (& normalized) stylings, industry share lowercase year, remove paired_line_plots; altair_utils init

---
+++ altair_utils.py
@@ -0,0 +1,91 @@
+import altair as alt
+
+color_transit_teal = "#006c69"
+color_value_transit_teal = alt.value(color_transit_teal)
+
+# pre, post, neutral/both
+color_covid_pre_post_neutral = ["#ff8d6b", "#63acc9", "#6d6d6d"]
+
+color_line_blue_green = ["#73abbe", "#cccc3d"]
+color_fill_blue_green = ["#5eb3e4", "#d3d655"]
+
+color_line_yellow_purple = ["#ffb81d", "#cd7f9e"]
+color_fill_yellow_purple = ["#fac32c", "#cd7f9e"]
+
+color_line_muni_bart_caltrain = ["#ff8d6b", "#8cb7c9", "#808080"]
+
+color_weekday_weekend = ["#5eb3e4", "#a0d0cb"]
+
+color_sf_bayarea = ["#358d89", "#bbbbbb"]
+color_sfcore_restofsf_restofbayarea = [color_transit_teal, "#6aaeaa", "#d2d2d2"]
+color_sfubercore_restofsfcore_restofsf = [
+    "#003b49"
+] + color_sfcore_restofsf_restofbayarea[:-1]
+
+color_collision_ped_bike = ["#cd7f9e", "#fea586"]
+
+color_line_modes = {
+    "DA": "#ff8d6d",  # also for all drive (DA + Carpool)
+    "Carpool": "#ffc6b2",  # i.e. HOV2 + HOV3+
+    "HOV2": "#f1a98c",
+    "HOV3+": "#f5bfa7",
+    "Taxi/TNC": "#f6cf3f",
+    "Bike": "#b9c279",
+    "Walk": "#8cb8ca",
+    "Transit": "#cd7f9e",
+    "Other": "#d2d2d2",
+    # for e.g. Walk+Bike+Other, pick the highest mode-share mode color
+    # (i.e. Walk in this case)
+    "Work from Home": "#6d6d6d",  # dashed line  # if fill: hashed
+}
+
+line_strokeWidth = 3
+line_strokeDash_dash = [line_strokeWidth * 4 / 3, line_strokeWidth * 8 / 3]
+line_strokeDash = [[1, 0], line_strokeDash_dash]
+
+
+def theme_sfcta():
+    return {
+        "config": {
+            "font": "Libre Franklin",
+            "axis": {
+                "labelAngle": 0,
+                "labelFlush": False,
+                "labelFontSize": 12,
+                "titleFontSize": 12,
+                # if we set "ticks": False, the spacing gets messed up
+                "tickWidth": 0,  # so we set the tickWidth to 0 instead
+                # "tickColor": "#cccccc",
+                "gridWidth": 0.25,
+                "gridColor": "#cccccc",
+            },
+            "axisX": {
+                "domainWidth": 0.5,
+                "domainColor": "#3e3e3e",
+                "titleAlign": "right",
+                "titleAnchor": "end",
+            },
+            "axisY": {
+                "domain": False,
+                # turn off for now since cannot figure out spacing:
+                # "labelAlign": "left",
+                "titleAlign": "left",
+                "titleAnchor": "end",
+                "titleAngle": 0,
+                "titlePadding": 0,
+                "titleY": -8,
+            },
+            "line": {"strokeWidth": line_strokeWidth, "strokeCap": "round"},
+            "legend": {"title": None},
+            "view": {"stroke": None},
+        }
+    }
+
+
+alt.themes.register("sfcta", theme_sfcta)
+alt.themes.enable("sfcta")
+
+
+# things to set for final html export:
+# - set axisY "ticksize" to set tick length so as to not have gridlines overlap labels
+# - axis title text a) allcaps, b) letter spacing (typographic "tracking")
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import altair as alt
 import polars as pl
 
 
@@ -21,33 +22,74 @@ def add_normalization_col(df, x, y, normalized_y_col_name, norm_x_value=2019):
 def add_industry_share_col(df, col):
     """for CES and QCEW"""
     return df.with_columns(
-        (pl.col(col) / pl.col(col).sum().over("Year", "geography")).alias(
+        (pl.col(col) / pl.col(col).sum().over("year", "geography")).alias(
             f"{col}-industry_share"
         )
     )
 
 
-def line_plot(df, x, y, by, title, frame_width=None, legend=True):
-    return df.sort(x).plot.line(
-        x=x, y=y, by=by, title=title, frame_width=frame_width, legend=legend
+def line_plot(df, x, y, color, title="", tooltip=[]):
+    if title == "":
+        title = y
+    if x in {"year", "Year"}:
+        df = df.with_columns(date=pl.date(pl.col(x), 1, 1))
+        x_axis = alt.X("date", title=x)
+    else:
+        x_axis = x
+    return (
+        alt.Chart(df.sort(x))
+        .mark_line()
+        .encode(x=x_axis, y=alt.Y(y, title=title), color=color, tooltip=tooltip)
+        .interactive()
     )
 
 
 def line_plot_normalized(
-    df, x, y, by, title, norm_x_value=2019, frame_width=None, legend=True
+    df,
+    x,
+    y,
+    color,
+    title=None,
+    norm_x_value=2019,
+    tooltip=[],
+    mark_line_args={},
+    chart_args={},
 ):
-    normalized_y_col_name = f"{y}-normalized_to_{norm_x_value}"
-    return line_plot(
-        add_normalization_col(
-            df, x, y, normalized_y_col_name, norm_x_value=norm_x_value
-        ),
-        x,
-        normalized_y_col_name,
-        by,
-        title=title,
-        frame_width=frame_width,
-        legend=legend,
-    )
+    normalized_y_col_name = f"{y} (% of {norm_x_value} value)"
+    normalized_df = add_normalization_col(
+        df, x, y, normalized_y_col_name, norm_x_value=norm_x_value
+    ).sort(x)
+    if title is None:
+        title = normalized_y_col_name
+    else:
+        title += f" (% of {norm_x_value} value)"
+    if x in {"year", "Year"}:  # make the x axis pretty
+        normalized_df = normalized_df.with_columns(date=pl.date(pl.col(x), 1, 1))
+        # don't show x axis because graph not starting at 0 on y axis
+        x_axis = alt.X("date:T", title=x, axis=alt.Axis(domain=False))
+    else:
+        # x domain = False: don't show x axis because graph not starting at 0 on y axis
+        x_axis = alt.X(x, domain=False, axis=alt.Axis(domain=False))
+    chart = (
+        alt.Chart(normalized_df)
+        .mark_line(**mark_line_args)
+        .encode(
+            x=x_axis,
+            y=alt.Y(normalized_y_col_name, title=title)
+            .scale(zero=False)
+            .axis(format="%"),
+            color=color,
+            tooltip=tooltip,
+            **chart_args,
+        )
+        + alt.Chart(pl.DataFrame({x: [norm_x_value]}))
+        .mark_rule(color="grey", opacity=0.5, strokeWidth=2)
+        .encode(x=x_axis)
+        + alt.Chart(pl.DataFrame({normalized_y_col_name: [1]}))
+        .mark_rule(color="grey", opacity=0.5, strokeWidth=2)
+        .encode(y=normalized_y_col_name)
+    ).interactive()
+    return normalized_df, chart
 
 
 def stacked_bar_plot(df, y, geography, by, title, years, frame_width=None, legend=True):
@@ -64,31 +106,3 @@ def stacked_bar_plot(df, y, geography, by, title, years, frame_width=None, legen
             legend=legend,
         )
     )
-
-
-def paired_line_plots(df, x, y, by, ymin=None, frame_width=None):
-    sf_plot = df.filter(pl.col("geography") == "San Francisco").plot.line(
-        x=x,
-        y=y,
-        by=by,
-        # stacked=True,
-        title="San Francisco",
-        ylim=(ymin, None),
-        frame_width=frame_width,
-        # frame_height=200,
-        xticks=df.select(x).unique(),
-        legend=False,
-    )
-    bayarea_plot = df.filter(pl.col("geography") == "Bay Area").plot.line(
-        x=x,
-        y=y,
-        by=by,
-        # stacked=True,
-        title="Bay Area",
-        ylim=(ymin, None),
-        frame_width=frame_width,
-        # frame_height=200,
-        xticks=df.select(x).unique(),
-        legend=True,
-    )
-    return sf_plot + bayarea_plot
